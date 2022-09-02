b56f6b54f24a130c5429691dd9bdcb0ccf009de7
nurul rizal <nurul.rizal@digiprimatera.co.id>
2022-09-02 20:25:13 +0700

    only string number and _dot-

--- app/Http/Controllers/Api/MeteraiController.php
+++ app/Http/Controllers/Api/MeteraiController.php
@@ -129,7 +129,7 @@ class MeteraiController extends Controller
                 if($user){
                     $request->validate([
                         'content' => 'required|array',
-                        'content.filename' => 'required|max:255',
+                        'content.filename' => 'required|max:255|regex:/^[a-zA-Z0-9_.-]+$/u',
                         'content.base64Doc' => 'required',
                         'content.docType' => 'required|max:255',
                         'content.signer' => 'array|min:1',
