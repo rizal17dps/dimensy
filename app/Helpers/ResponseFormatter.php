e828ef74ad28c6ea2e668af57ba9402e9d115ecb
nurul rizal <nurul.rizal@digiprimatera.co.id>
2023-01-19 13:58:59 +0700

    ubah getenv jadi $_server

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -56,18 +56,18 @@ class ResponseFormatter
 
     public static function get_client_ip() {
         $ipaddress = '';
-        if (getenv('HTTP_CLIENT_IP'))
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-           $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
-            $ipaddress = getenv('REMOTE_ADDR');
+        if ($_SERVER('HTTP_CLIENT_IP'))
+            $ipaddress = $_SERVER('HTTP_CLIENT_IP');
+        else if($_SERVER('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = $_SERVER('HTTP_X_FORWARDED_FOR');
+        else if($_SERVER('HTTP_X_FORWARDED'))
+            $ipaddress = $_SERVER('HTTP_X_FORWARDED');
+        else if($_SERVER('HTTP_FORWARDED_FOR'))
+            $ipaddress = $_SERVER('HTTP_FORWARDED_FOR');
+        else if($_SERVER('HTTP_FORWARDED'))
+           $ipaddress = $_SERVER('HTTP_FORWARDED');
+        else if($_SERVER('REMOTE_ADDR'))
+            $ipaddress = $_SERVER('REMOTE_ADDR');
         else
             $ipaddress = \Request::ip();
         return $ipaddress;
